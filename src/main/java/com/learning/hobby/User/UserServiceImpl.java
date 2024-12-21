dad25c36684d68ab89e3a14d8a33d156dcc29b3b
Brandon <branyzp@gmail.com>
2024-12-21 11:44:54 +0800

    finish up register and login

--- src/main/java/com/learning/hobby/User/UserServiceImpl.java
+++ src/main/java/com/learning/hobby/User/UserServiceImpl.java
@@ -1,16 +1,37 @@
 package com.learning.hobby.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public void register(UserRegisterDTO userRegisterDTO) {
+        if (userRepository.findByEmail(userRegisterDTO.getEmail()).isPresent() || userRepository.findByUsername(userRegisterDTO.getUsername()).isPresent()) {
+            throw new RuntimeException("Username or Email already exists in the system.");
+        }
+        User user = new User();
+        user.setUsername(userRegisterDTO.getUsername());
+        user.setEmail(userRegisterDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(userRegisterDTO.getPassword()));
 
+        userRepository.save(user);
     }
 
     @Override
     public String login(UserLoginDTO userLoginDTO) {
-        return "";
+        User user = userRepository.findByUsername(userLoginDTO.getUsername()).orElseThrow(()->new RuntimeException("Invalid username or password"));
+        if (!passwordEncoder.matches(userLoginDTO.getPassword(),user.getPassword())) {
+            throw new RuntimeException("Invalid username or password");
+        }
+        return "Login successful";
     }
 }
